add51e7ab4acaf755cf6bafefd03b50e65173ecb
Michael Gherea <mikey997@gmail.com>
2024-06-07 11:22:00 -0700

    Populated home page, added gallery and banner with placeholders

--- exampleportfolio/src/components/Home.tsx
+++ exampleportfolio/src/components/Home.tsx
@@ -1,6 +1,16 @@
 import React from "react";
-export default function Navbar() {
+import "./Home.css"
+import GalleryWrapper from "./GalleryWrapper";
+export default function Home() {
     return (
-      <p>I'm home</p>
+      <div className="Homediv">
+        <div className="Banner">
+          <img className="BannerImage" src="https://picsum.photos/id/1018/1000/600/" alt="Banner"></img>
+          <div className="BannerText">
+            <h2>Lol Lmao even</h2>
+          </div>
+        </div>
+        <GalleryWrapper />
+      </div>
     )
   }
